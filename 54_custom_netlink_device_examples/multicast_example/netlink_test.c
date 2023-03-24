ba8edca777c5583f38152c79a778f1940ac5938c
Neelkanth Reddy <www.neelkanth.13@gmail.com>
2023-03-24 18:02:08 +0530

    Add files via upload

--- 54_custom_netlink_device_examples/multicast_example/netlink_test.c
+++ 54_custom_netlink_device_examples/multicast_example/netlink_test.c
@@ -6,6 +6,29 @@
 #define MYPROTO NETLINK_USERSOCK
 #define MYGRP 17
 
+/*
+ * In this project, we have 3 userspace processes and 1 kernel device driver.
+All of them are part of the netlink multicast group.
+one userspace process nl_send sends message to other 2 userspace processes
+and linux kernel driver.
+
+Now, If the three userspace processes are subscribed to the same multicast group
+using the same netlink socket and are part of the same multicast group, then
+when one of the userspace processes sends a message to the multicast group,
+the message will be delivered to all the other subscribed processes via the
+kernel driver.
+
+In a multicast group, the kernel driver acts as a message router, forwarding the
+messages to all the subscribed processes. When a process sends a message to the
+multicast group, the message is sent to the kernel driver, which then delivers the
+message to all the subscribed processes.
+
+Therefore, when one of the userspace processes sends a
+message to the multicast group, the message will be delivered to the kernel driver,
+which will then forward the message to all the subscribed processes, including the
+other two userspace processes.
+ */
+
 struct sock *nl_sock = NULL;
 
 static void netlink_test_recv_msg(struct sk_buff *skb)
